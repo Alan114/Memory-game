04d8f16a9f7d6682daaebacc82ead74a494a5679
Alan Pecenkovic <alanquad@yahoo.com>
2022-03-15 11:11:37 +0100

    Displaying the number of turns and starting the game when the app renders

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null);
+    setChoiceTwo(null);
     setCards(shuffledCards);
     setTurns(0);
   };
@@ -64,6 +66,11 @@ function App() {
     setDisabled(false);
   };
 
+  // start the game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className="App">
       <h1>Memory game</h1>
@@ -80,6 +87,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
