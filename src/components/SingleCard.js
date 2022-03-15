a5f34206e6adbd4fde33f4661bf143de53beaa66
Alan Pecenkovic <alanquad@yahoo.com>
2022-03-15 08:38:21 +0100

    Making two states for card choices, adding a handleChoice function which is passed from App.js to Singlecard.js through props and setting the choosen cards states

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 import "./SingleCard.css";
 
 const SingleCard = (props) => {
+  const handleClick = () => {
+    props.handleChoice(props.card);
+  };
   return (
     <div className="card">
       <div>
         <img className="front" src={props.card.src} alt="card front" />
-        <img className="back" src="/img/cover.png" alt="card back" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="card back"
+        />
       </div>
     </div>
   );
